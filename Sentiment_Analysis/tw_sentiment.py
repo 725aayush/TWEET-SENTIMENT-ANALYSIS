c7c748a0f565c58097dab0b3f531d5945b185496
Aayush Pareek <153154221+725aayush@users.noreply.github.com>
2026-01-05 10:57:09 +0530

    Refactor sentiment analysis for improved clarity

--- Sentiment_Analysis/tw_sentiment.py
+++ Sentiment_Analysis/tw_sentiment.py
@@ -1,32 +1,28 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from scipy.special import softmax
+import torch
 
 def preprocess_tweet(tweet):
     tweet_words = []
-    for word in tweet.split(' '):
+    for word in tweet.split():
         if word.startswith('@') and len(word) > 1:
             word = '@user'
         elif word.startswith('http'):
-            word = "http"
+            word = 'http'
         tweet_words.append(word)
     return " ".join(tweet_words)
 
-# Load model and tokenizer globally to avoid reloading them for each request
 roberta = "cardiffnlp/twitter-roberta-base-sentiment"
-model = AutoModelForSequenceClassification.from_pretrained(roberta)
 tokenizer = AutoTokenizer.from_pretrained(roberta)
-labels = ['Negative', 'Neutral', 'Positive']
+model = AutoModelForSequenceClassification.from_pretrained(roberta)
+labels = ['negative', 'neutral', 'positive']
 
 def analyze_sentiment(tweet):
     tweet_proc = preprocess_tweet(tweet)
-    encoded_tweet = tokenizer(tweet_proc, return_tensors='pt')
-    output = model(**encoded_tweet)
-    scores = output[0][0].detach().numpy()
-    scores = softmax(scores)
-    
-    result = {labels[i].lower(): round(float(scores[i]) * 100, 2) for i in range(len(scores))}
-    return result
+    encoded = tokenizer(tweet_proc, return_tensors="pt")
 
-if __name__ == "__main__":
-    tweet = 'it heartbreaking about the news !!'
-    print(analyze_sentiment(tweet))
+    with torch.no_grad():
+        output = model(**encoded)
+
+    scores = softmax(output.logits[0].numpy())
+    return {labels[i]: round(float(scores[i]) * 100, 2) for i in range(3)}
